f856b795c22749f881b417fd4fd9900fb25b41df
Helina Tefera <80898448+helinatefera@users.noreply.github.com>
2023-07-16 04:34:25 -0700

    Delete app directory

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('front/',views.front,name='front'),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('api/', views.brain_api, name='api')
-]
